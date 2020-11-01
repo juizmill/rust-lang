ce20709d84b4dda612d316795cbd49cfba5b0354
Jesus Vieira <jesusvieiradelima@gmail.com>
2020-11-01 12:22:57 -0300

    Entendendo como funciona sobrecarda..

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,15 @@ fn main() {
 
     x = 6;
     println!("The value of x is: {}", x);
+
+    let a = 5;
+    let a = a + 1;
+    let a = a * 2;
+
+    println!("The value of a is: {}", a);
+
+    let spaces = " ";
+    let spaces = spaces.len();
+
+    println!("The value of spaces is: {}", spaces);
 }
