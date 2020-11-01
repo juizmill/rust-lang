4060f253cda4f80955c13405e251b1f5e8275ed5
Jesus Vieira <jesusvieiradelima@gmail.com>
2020-11-01 12:49:45 -0300

    Tipos de ponto flutuante

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,25 @@
 fn main() {
     println!("Hello, World");
 
-    let mut x = 5;
+    let mut x: i32 = 5;
     println!("The value of x is: {}", x);
 
     x = 6;
     println!("The value of x is: {}", x);
 
-    let a = 5;
-    let a = a + 1;
-    let a = a * 2;
+    let a: i32 = 5;
+    let a: i32 = a + 1;
+    let a: i32 = a * 2;
 
     println!("The value of a is: {}", a);
 
-    let spaces = " ";
-    let spaces = spaces.len();
+    let spaces: &str = " ";
+    let spaces: usize = spaces.len();
 
     println!("The value of spaces is: {}", spaces);
+
+    let b = 2.8;
+    let c: f32 = 3.10;
+
+    println!("The value of b is: {} and c is: {}", b, c);
 }
